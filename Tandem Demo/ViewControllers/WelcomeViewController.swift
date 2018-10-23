524011a239ce23b89e7fc0f75268cf83f6ee1264
Yaroslav Stupinskyi <jaros.sv@gmail.com>
2018-10-23 09:29:20 +0300

    Add comments & structuring

--- Tandem Demo/ViewControllers/WelcomeViewController.swift
+++ Tandem Demo/ViewControllers/WelcomeViewController.swift
@@ -96,7 +96,6 @@ class WelcomeViewController: UIViewController {
             self.userImageSelectionEnded()
             self.registerModeEnded()
         }
-        
     }
     
     func textFieldReturn(textField: UITextField) -> Bool {
@@ -142,3 +141,199 @@ extension WelcomeViewController: UIImagePickerControllerDelegate, UINavigationCo
     }
 }
 
+extension WelcomeViewController {
+    
+    //MARK: - Login input checking
+    func checkUserLoginInput() {
+        if usersDataDictionary.keys.contains(logInTextField.text!) == false {
+            AlertView.shared.showAlert(fromController: self, withTitle: "Login is incorrect", message: "Please provide your correct e-mail adress")
+            SVProgressHUD.dismiss()
+            
+        } else if usersDataDictionary[logInTextField.text!] != passwordTextField.text {
+            print(usersDataDictionary[logInTextField.text!]!)
+            AlertView.shared.showAlert(fromController: self, withTitle: "Password is incorrect", message: "Please provide valid password")
+            SVProgressHUD.dismiss()
+            
+        } else {
+            guard let login = logInTextField.text else { return }
+            guard let password = passwordTextField.text else { return }
+            DataService.shared.signIn(withEmail: login, password: password, completion: { (isSuccess) in
+                if isSuccess == true {
+                    //MARK: - Loading preferred color settings of current user and posting notifications
+                    DataService.shared.loadColorThemeSetting(completion: { (colorTheme) in })
+                    SVProgressHUD.dismiss()
+                    self.logInModeEnded()
+                } else {
+                    print("Error signing in")
+                }
+            })
+        }
+    }
+    
+    //MARK: - Registration input check
+    func checkUserRegistrationInput() {
+        if loginRegisterTextField.text == "" {
+            AlertView.shared.showAlert(fromController: self, withTitle: "Login field is empty", message: "Please provide your e-mail adress")
+            
+        } else if loginRegisterTextField.text?.contains("@") == false || loginRegisterTextField.text?.contains(".") == false ||
+            loginRegisterTextField.text?.contains("com") == false {
+            AlertView.shared.showAlert(fromController: self, withTitle: "Incorrect e-mail", message: "The e-mail adress is badly formatted")
+            
+        } else if passwordRegisterTextField.text != verifyPasswordTextField.text {
+            AlertView.shared.showAlert(fromController: self, withTitle: "Password missmatch", message: "Please provide and verify valid password")
+            
+        } else if (passwordRegisterTextField.text?.count)! < 6 {
+            AlertView.shared.showAlert(fromController: self, withTitle: "Password is too short", message: "Password should be at least 6 characters long")
+            
+        } else {
+            userImageSelection()
+        }
+    }
+}
+
+extension WelcomeViewController {
+    
+    //MARK: - Login layout mode
+    func logInModeBegin() {
+        loginStackView.isHidden = false
+        logInTextField.isHidden = false
+        loginLabel.isHidden = false
+        passwordTextField.isHidden = false
+        passwordLabel.isHidden = false
+        
+        logInButton.isHidden = true
+        logInButton.isEnabled = false
+        registerButton.isHidden = true
+        registerButton.isEnabled = false
+        
+        confirmButton.isHidden = false
+        confirmButton.isEnabled = true
+        backLoginButton.isHidden = false
+        backLoginButton.isEnabled = true
+    }
+    
+    func logInModeEnded() {
+        loginStackView.isHidden = true
+        logInTextField.isHidden = true
+        logInTextField.text = ""
+        loginLabel.isHidden = true
+        passwordTextField.isHidden = true
+        passwordTextField.text = ""
+        passwordLabel.isHidden = true
+        
+        logInButton.isHidden = false
+        logInButton.isEnabled = true
+        registerButton.isHidden = false
+        registerButton.isEnabled = true
+        
+        confirmButton.isHidden = true
+        confirmButton.isEnabled = false
+        backLoginButton.isHidden = true
+        backLoginButton.isEnabled = false
+    }
+    
+    //MARK: - Register layout mode
+    func registerModeBegin() {
+        registrationStackView.isHidden = false
+        loginRegisterLabel.isHidden = false
+        loginRegisterTextField.isHidden = false
+        passwordRegisterLabel.isHidden = false
+        passwordRegisterTextField.isHidden = false
+        verifyPasswordLabel.isHidden = false
+        verifyPasswordTextField.isHidden = false
+        
+        logInButton.isHidden = true
+        logInButton.isEnabled = false
+        registerButton.isHidden = true
+        registerButton.isEnabled = false
+        
+        nextRegisterButton.isHidden = false
+        nextRegisterButton.isEnabled = true
+        backRegisterButton.isHidden = false
+        backRegisterButton.isEnabled = true
+    }
+    
+    func registerModeEnded() {
+        registrationStackView.isHidden = true
+        loginRegisterLabel.isHidden = true
+        loginRegisterTextField.isHidden = true
+        loginRegisterTextField.text = ""
+        passwordRegisterLabel.isHidden = true
+        passwordRegisterTextField.isHidden = true
+        passwordRegisterTextField.text = ""
+        verifyPasswordLabel.isHidden = true
+        verifyPasswordTextField.isHidden = true
+        verifyPasswordTextField.text = ""
+        
+        logInButton.isHidden = false
+        logInButton.isEnabled = true
+        registerButton.isHidden = false
+        registerButton.isEnabled = true
+        
+        nextRegisterButton.isHidden = true
+        nextRegisterButton.isEnabled = false
+        backRegisterButton.isHidden = true
+        backRegisterButton.isEnabled = false
+    }
+    
+    //MARK: - Image selection layout mode
+    func userImageSelection() {
+        registrationStackView.isHidden = true
+        loginRegisterLabel.isHidden = true
+        loginRegisterTextField.isHidden = true
+        passwordRegisterLabel.isHidden = true
+        passwordRegisterTextField.isHidden = true
+        verifyPasswordLabel.isHidden = true
+        verifyPasswordTextField.isHidden = true
+        
+        userImageView.isHidden = false
+        chooseImageButton.isHidden = false
+        chooseImageButton.isEnabled = true
+        chooseImageButton.layer.borderWidth = 1
+        chooseImageButton.layer.borderColor = UIColor.white.cgColor
+        chooseImageButton.layer.cornerRadius = chooseImageButton.frame.width / 2
+        userImageView.layer.cornerRadius = userImageView.frame.width / 2
+        
+        nextRegisterButton.isHidden = true
+        nextRegisterButton.isEnabled = false
+        backRegisterButton.isHidden = true
+        backRegisterButton.isEnabled = false
+        
+        backUserImageButton.isHidden = false
+        backUserImageButton.isEnabled = true
+        
+        submitButton.isHidden = false
+        submitButton.isEnabled = true
+    }
+    
+    func userImageSelectionEnded() {
+        registrationStackView.isHidden = false
+        loginRegisterLabel.isHidden = false
+        loginRegisterTextField.isHidden = false
+        passwordRegisterLabel.isHidden = false
+        passwordRegisterTextField.isHidden = false
+        verifyPasswordLabel.isHidden = false
+        verifyPasswordTextField.isHidden = false
+        
+        logInButton.isHidden = true
+        logInButton.isEnabled = false
+        registerButton.isHidden = true
+        registerButton.isEnabled = false
+        
+        userImageView.image = nil
+        userImageView.isHidden = true
+        chooseImageButton.isHidden = true
+        chooseImageButton.isEnabled = false
+        
+        nextRegisterButton.isHidden = false
+        nextRegisterButton.isEnabled = true
+        backRegisterButton.isHidden = false
+        backRegisterButton.isEnabled = true
+        
+        backUserImageButton.isHidden = true
+        backUserImageButton.isEnabled = false
+        
+        submitButton.isHidden = true
+        submitButton.isEnabled = false
+    }
+}
